22727e734a8b2af3031c698c0da250c33d1b0308
frostice482 <frostice48260@gmail.com>
2025-09-05 04:16:05 +0700

    initial

---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a tiny version of https://github.com/skyline69/balatro-mod-index.
+The index repo contains lots of `thumbnail.jpg` files, which fills up the repo size significantly.
+This repo aims to reduce the repo size by not including the `thumbnail.jpg` by default, and reducing API calls by bundling mod metadata to one file.
---
+++ run.js
@@ -0,0 +1,39 @@
+const cp = require('child_process')
+const fs = require('fs')
+const fsp = require('fs/promises')
+const util = require('util')
+const zlib = require('zlib')
+
+const cp_exec_prm = util.promisify(cp.exec)
+
+async function main() {
+    process.chdir('bmi')
+    const t = new Date()
+
+    if (!fsp.stat('bmi').catch(() => {}))
+        await cp_exec_prm('git clone https://github.com/skyline69/balatro-mod-index bmi')
+
+    await cp_exec_prm('git pull')
+
+    const items = await fsp.readdir('mods')
+    const metas = items.map(v => fsp.readFile(`mods/${v}/meta.json`))
+
+    const fout = fs.createWriteStream('../out.json.gz')
+    const out = zlib.createGzip()
+    out.pipe(fout)
+
+    out.write('[')
+    for (const [i, content] of metas.entries()) {
+        if (i != 0) out.write(',')
+        out.write(await content)
+    }
+    out.write(']')
+
+    await new Promise(res => out.end(res))
+
+    process.chdir('..')
+
+    await cp_exec_prm('git add out.json.gz')
+    await cp_exec_prm(`git commit -m "auto @ ${t.toUTCString()}"`)
+}
+main()

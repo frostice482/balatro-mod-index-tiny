ad0f4ccf9b865b3e706eb77c084f8dbbc5c10725
frostice482 <frostice48260@gmail.com>
2025-09-06 00:52:56 +0700

    format change - changed from object to array - added property pathname, id, version

--- run.js
+++ run.js
@@ -1,15 +1,165 @@
+const http = require('http')
 const cp = require('child_process')
-const fs = require('fs')
 const fsp = require('fs/promises')
 const util = require('util')
 const zlib = require('zlib')
 
+const cp_exec_prm = util.promisify(cp.exec)
+const gzip = util.promisify(zlib.gzip)
 process.chdir(__dirname)
 
-const cp_exec_prm = util.promisify(cp.exec)
+const metaFieldTypes = {
+    id: "string",
+    name: "string",
+    description: "string",
+    prefix: "string",
+    author: "object",
+    version: "string"
+}
+
+const tsManifestFieldTypes = {
+    name: "string",
+    version_number: "string",
+    website_url: "string",
+    description: "string",
+    dependencies: "object"
+}
+
+const headerFields = {
+    MOD_ID: "id",
+    //MOD_NAME: "name",
+    //MOD_DESCRIPTION: "description",
+    //PREFIX: "prefix",
+    VERSION: "version",
+}
+
+const delprops = [
+    'requires-steamodded',
+    'requires-talisman',
+    'automatic-version-check',
+    'fixed-release-tag-updates',
+    'last-updated',
+    'foldername',
+    'version'
+]
+
+function fieldTypeSatisfy(obj, fieldTypes) {
+    for (const [k,v] of Object.entries(fieldTypes)) {
+        if (typeof(obj[k]) != v) return false
+    }
+    return true
+}
+
+async function handleJsonInfo(entry) {
+    const isJson = entry.name.endsWith(".json")
+    const isLua = entry.name.endsWith(".lua")
+
+    if (!(
+        entry.type == "file"
+        && entry.name[0] != "."
+        && ( isJson || isLua )
+    )) return
+
+    const res = await fetch(entry.download_url)
+    if (res.status != 200) throw Error(`${res.url} HTTP ${res.status}`)
+
+    let obj, fmt
+
+    if (isJson) {
+        const data = await res.json()
+        if (fieldTypeSatisfy(data, metaFieldTypes)) {
+            obj = data
+            fmt = 'smods'
+            //for (const prop of metaDelProps) delete obj[prop]
+        }
+        else if (entry.name == "manifest.json" && fieldTypeSatisfy(data, tsManifestFieldTypes)) {
+            obj = data
+            fmt = 'thunderstore'
+            //for (const prop of tsDelProps) delete obj[prop]
+        }
+    }
+    else if (isLua) {
+        const data = await res.text()
+        const lines = data.split(/\r?\n/)
+        if (lines[0] != '--- STEAMODDED HEADER') return
+
+        obj = {}
+        fmt = 'smods-header'
+        for (const line of lines) {
+            const m = line.match(/^--- *(\w+) *:(.*)/)
+            if (!m) break
+            const [_, prop, val] = m
+            if (!headerFields[prop])
+            obj[headerFields[prop]] = val
+        }
+    }
+
+    return {
+        obj: obj,
+        format: fmt
+    }
+}
+
+async function getJsonInfo(host, repo) {
+    let res
+    if (host == "github.com") {
+        res = await fetch(`https://api.github.com/repos/${repo}/contents`, {
+            headers: {
+                Authorization: 'Bearer ' + process.env.GITHUB_TOKEN
+            }
+        })
+    } else if (host == "codeberg.org") {
+        res = await fetch(`https://${host}/api/v1/repos/${repo}/contents`)
+    }
+
+    if (res.status != 200) throw Error(`${res.url} HTTP ${res.status}`)
+
+    const list = await res.json()
+    for (const entry of list) {
+        const data = await handleJsonInfo(entry)
+        if (data) return data
+    }
+}
+
+async function handleItem(name) {
+    const content = await fsp.readFile(`bmi/mods/${name}/meta.json`)
+    const data = JSON.parse(content)
+    data.pathname = name
+
+    for (const prop of delprops) delete data[prop]
+
+    const m = data.repo.match(/^https:\/\/([\w.]+)\/([\w.-]+\/[\w.-]+)/)
+    if (m) {
+        const meta = await getJsonInfo(m[1], m[2])
+        if (meta) {
+            switch (meta.format) {
+                case 'smods':
+                case 'smods-header':
+                    data.id = meta.obj.id
+                    data.version = meta.obj.version
+                    break
+                case 'thunderstore':
+                    data.id = meta.obj.name
+                    data.version = meta.obj.version_number
+                    break
+            }
+            //data.meta = meta.obj
+        }
+    }
+
+    return data
+}
+
+async function initEnv() {
+    const envstr = await fsp.readFile('.env', 'utf-8')
+    for (const entry of envstr.split('\n')) {
+        const [name] = entry.match(/^[\w_]+/)
+        process.env[name] = entry.slice(name.length + 1)
+    }
+}
 
 async function main() {
-    process.chdir('bmi')
+    await initEnv()
     const t = new Date()
 
     if (!fsp.stat('bmi').catch(() => {}))
@@ -17,24 +167,21 @@ async function main() {
 
     await cp_exec_prm('git pull')
 
-    const items = await fsp.readdir('mods')
-    const metas = items.map(v => [v, fsp.readFile(`mods/${v}/meta.json`)])
+    const items = await fsp.readdir('bmi/mods')
 
-    const fout = fs.createWriteStream('../out.json.gz')
-    const out = zlib.createGzip()
-    out.pipe(fout)
-
-    out.write('{')
-    for (const [i, [name, content]] of metas.entries()) {
-        if (i != 0) out.write(',')
-        out.write(JSON.stringify(name) + ":")
-        out.write(await content)
+    const metaResolves = await Promise.allSettled(items.map(handleItem))
+    const metas = []
+    for (const res of metaResolves) {
+        if (res.status == 'rejected')
+            console.warn(res.reason)
+        else
+            metas.push(res.value)
     }
-    out.write('}')
 
-    await new Promise(res => out.end(res))
+    const str = JSON.stringify(metas, null)
 
-    process.chdir('..')
+    await fsp.writeFile('out.json', str)
+    await gzip(str).then(v => fsp.writeFile('out.json.gz', v))
 
     await cp_exec_prm('git add out.json.gz')
     await cp_exec_prm(`git commit -m "auto @ ${t.toUTCString()}"`)
